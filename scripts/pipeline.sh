3a9a9824ffd6e5ab9a56f93ef2eac639f0cb2d40
WhatsARanjit <whatsaranjit@gmail.com>
2020-01-21 21:39:30 -0500

    Adding another curly

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -6,7 +6,7 @@ export KEY='deadline'
 export VALUE=$(date --utc +%FT%TZ)
 curl -v -sk \
   --request PATCH \
-  --data "{\"data\":{\"id\":\"$VAR_ID\",\"attributes\":{\"key\":\"$KEY\",\"value\":\"$VALUE\",\"category\":\"policy-set\",\"description\":null,\"hcl\":false,\"sensitive\":false,\"read-only\":false,\"relationships\":{\"configurable\":{\"data\":{\"type\":\"policy-sets\",\"id\":\"$POLICY_ID\"}}},\"type\":\"vars\"}}" \
+  --data "{\"data\":{\"id\":\"$VAR_ID\",\"attributes\":{\"key\":\"$KEY\",\"value\":\"$VALUE\",\"category\":\"policy-set\",\"description\":null,\"hcl\":false,\"sensitive\":false,\"read-only\":false,\"relationships\":{\"configurable\":{\"data\":{\"type\":\"policy-sets\",\"id\":\"$POLICY_ID\"}}},\"type\":\"vars\"}}}" \
   -H "Content-type: application/vnd.api+json" \
   -H "Authorization: Bearer $TFE_TOKEN" \
   https://app.terraform.io/api/v2/policy-sets/$POLICY_ID/parameters/$VAR_ID
@@ -18,7 +18,7 @@ export KEY='version'
 export VALUE=$(echo $TRAVIS_TAG | sed -E 's/v?([0-9\.]+)/\1/')
 curl -v -sk \
   --request PATCH \
-  --data "{\"data\":{\"id\":\"$VAR_ID\",\"attributes\":{\"key\":\"$KEY\",\"value\":\"$VALUE\",\"category\":\"policy-set\",\"description\":null,\"hcl\":false,\"sensitive\":false,\"read-only\":false,\"relationships\":{\"configurable\":{\"data\":{\"type\":\"policy-sets\",\"id\":\"$POLICY_ID\"}}},\"type\":\"vars\"}}" \
+  --data "{\"data\":{\"id\":\"$VAR_ID\",\"attributes\":{\"key\":\"$KEY\",\"value\":\"$VALUE\",\"category\":\"policy-set\",\"description\":null,\"hcl\":false,\"sensitive\":false,\"read-only\":false,\"relationships\":{\"configurable\":{\"data\":{\"type\":\"policy-sets\",\"id\":\"$POLICY_ID\"}}},\"type\":\"vars\"}}}" \
   -H "Content-type: application/vnd.api+json" \
   -H "Authorization: Bearer $TFE_TOKEN" \
   https://app.terraform.io/api/v2/policy-sets/$POLICY_ID/parameters/$VAR_ID
