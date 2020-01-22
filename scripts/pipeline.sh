dac09adabddc4025be90f6090741c0b893fa12da
WhatsARanjit <whatsaranjit@gmail.com>
2020-01-21 21:30:17 -0500

    Fixed curly bug; adding verbosity

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -4,9 +4,9 @@
 export VAR_ID='var-AM5RdVHHmUmjbnVe'
 export KEY='deadline'
 export VALUE=$(date --utc +%FT%TZ)
-curl -sk \
+curl -v -sk \
   --request PATCH \
-  --data "{\"data\":{\"id\":\"$VAR_ID\",\"attributes\":{\"key\":\"$KEY\",\"value\":\"$VALUE\",\"category\":\"policy-set\",\"description\":null,\"hcl\":false,\"sensitive\":false,\"read-only\":false,\"relationships\":{\"configurable\":{\"data\":{\"type\":\"policy-sets\",\"id\":\"$POLICY_ID\"}}},\"type\":\"vars\"}}" \
+  --data "{\"data\":{\"id\":\"$VAR_ID\",\"attributes\":{\"key\":\"$KEY\",\"value\":\"$VALUE\",\"category\":\"policy-set\",\"description\":null,\"hcl\":false,\"sensitive\":false,\"read-only\":false,\"relationships\":{\"configurable\":{\"data\":{\"type\":\"policy-sets\",\"id\":\"$POLICY_ID\"}}},\"type\":\"vars\"}}}" \
   -H "Content-type: application/vnd.api+json" \
   -H "Authorization: Bearer $TFE_TOKEN" \
   https://app.terraform.io/api/v2/policy-sets/$POLICY_ID/parameters/$VAR_ID
@@ -16,7 +16,7 @@ curl -sk \
 export VAR_ID='var-z9zxfNbrcvJjCNJB'
 export KEY='version'
 export VALUE=$(echo $TRAVIS_TAG | sed -E 's/v?([0-9\.]+)/\1/')
-curl -sk \
+curl -v -sk \
   --request PATCH \
   --data "{\"data\":{\"id\":\"$VAR_ID\",\"attributes\":{\"key\":\"$KEY\",\"value\":\"$VALUE\",\"category\":\"policy-set\",\"description\":null,\"hcl\":false,\"sensitive\":false,\"read-only\":false,\"relationships\":{\"configurable\":{\"data\":{\"type\":\"policy-sets\",\"id\":\"$POLICY_ID\"}}},\"type\":\"vars\"}}" \
   -H "Content-type: application/vnd.api+json" \
